7a9186a66e7abf3d9a08ee8d7cd73046214a3664
Danish Mohammad <dmoham7@uwo.ca>
2025-09-02 04:19:12 -0400

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const {crawlPage} = require('./crawl.js')
+function main(){
+    if (process.argv.length < 3) {
+        console.log("no URL provided")
+        process.exit(1)
+    }
+    if (process.argv.length > 3) {
+        console.log("too many arguments provided")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+    console.log(`starting crawl of ${baseURL}`)
+    crawlPage(baseURL)
+}
+main()
